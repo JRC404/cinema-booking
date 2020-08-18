199c5a2ca7fe0f1cea240e5a2c6f6248a15255fc
Hamza <hamzam1478@gmail.com>
2020-08-18 11:50:37 +0100

    #2 Refreshments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 let twoD = 200
 let threeD = 150
 let both = 300
@@ -15,13 +14,13 @@ class order {
     } 
 
     price() {
-        if (age <= 7) {
+        if (this.age <= 7) {
             return("Your child ticket price will be £3")
         }
-        else if (age >= 7 && age <= 65) {
+        else if (this.age >= 7 && this.age <= 65) {
                 return("Your adult ticket price will be £10")
         }
-        else if (age >= 65) { 
+        else if (this.age >= 65) { 
             return("Your senior ticket price will be £5")
 
         }
@@ -31,3 +30,8 @@ class order {
     }
 }
 
+//* TEST CODE
+
+const newPerson = new order ("James", 67)
+
+console.log(newPerson.price())
