e89194a572be6cbd19eacb6fedf9788556b14f06
Alex Arditti <alex.arditti@hotmail.com>
2020-08-19 15:25:18 +0100

    #11 testing
    added const ticket to tests

--- tests/main.test.js
+++ tests/main.test.js
@@ -6,6 +6,31 @@
 }); */
 
 const Order = require("../src/index");
+test('Your child ticket price will be £3', () => {
+  const ticket = new order ("James", 6, "no") 
+  expect(ticket.price).toBe("Your child ticket price will be £3");
+
+});
+
+test('Your adult ticket price will be £10', () => {
+  const ticket = new order ("James", 36, "no") 
+  expect(ticket.price).toBe("Your adult ticket price will be £10");
+
+}); 
+
+test('Your student price will be £6', () => {
+  const ticket = new order ("James", 24, "yes") 
+  expect(ticket.price).toBe("Your student price will be £6");
+
+}); 
+
+test('Your senior ticket price will be £5', () => {
+  const ticket = new order ("James", 66, "no") 
+  expect(ticket.price).toBe("Your senior ticket price will be £5");
+
+});
+
+
 
 test(`asserting "name" is "Steven"`, () => {
   const bluemonday = new Order('Steven');
