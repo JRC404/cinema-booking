1c23b901739892bc4584da65713585081e10e124
Alex Arditti <alex.arditti@hotmail.com>
2020-08-19 15:42:57 +0100

    #11 testing
    fixed capital O on order class

--- tests/main.test.js
+++ tests/main.test.js
@@ -7,25 +7,25 @@
 
 const Order = require("../src/index");
 test('Your child ticket price will be £3', () => {
-  const ticket = new order ("James", 6, "no") 
+  const ticket = new Order ("James", 6, "no") 
   expect(ticket.price).toBe("Your child ticket price will be £3");
 
 });
 
 test('Your adult ticket price will be £10', () => {
-  const ticket = new order ("James", 36, "no") 
+  const ticket = new Order ("James", 36, "no") 
   expect(ticket.price).toBe("Your adult ticket price will be £10");
 
 }); 
 
 test('Your student price will be £6', () => {
-  const ticket = new order ("James", 24, "yes") 
+  const ticket = new Order ("James", 24, "yes") 
   expect(ticket.price).toBe("Your student price will be £6");
 
 }); 
 
 test('Your senior ticket price will be £5', () => {
-  const ticket = new order ("James", 66, "no") 
+  const ticket = new Order ("James", 66, "no") 
   expect(ticket.price).toBe("Your senior ticket price will be £5");
 
 });
